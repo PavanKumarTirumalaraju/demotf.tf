ee8aee9a51895387a0b36a05d8786ed0530ad983
ptirumalaraju1232 <124945192+ptirumalaraju1232@users.noreply.github.com>
2023-04-05 20:28:30 -0500

    Update provisions.tf

--- provisions.tf
+++ provisions.tf
@@ -19,32 +19,7 @@ resource "aws_instance" "web" {
   ami             = "ami-04581fbf744a7d11f"
   instance_type   = "t2.micro"
   tags            = local.common_tags
-  key_name        = "nnnn1"
-  vpc_security_group_ids = [data.aws_security_group.testsg.id]
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("./nnnn1.pem")
-
-    host = self.public_ip
-  }
-  provisioner "remote-exec" {
-        on_failure = continue
-    inline = [
-        
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key",
-
-      "sudo amazon-linux-extras install epel -y",
-      "sudo amazon-linux-extras install java-openjdk11 -y",
-      "sudo yum install jenkins -y",
-      "sudo service jenkins start",
-      "sudo cat  /var/lib/jenkins/secrets/initialAdminPassword"
-    ]
-  }
-
-}
-
+  
 
 
 
