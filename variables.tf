f38aa4bc8542f65ac9fc559ba139316f408b8495
Pavan Kumar Raju Tirumalaraju <pavantirumalaraju23@gmail.com>
2023-07-08 01:07:34 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,1 @@
-variable "instancetype" {
-    default = "t2.micro"
-  
-}
 
-variable "lists" {
-    type = list
-    description = "avialbility zones listings"
-    default =  ["t2.micro","t2.nano","t2.large"]
-}
-
-variable "maps" {
-    type = map 
-    description = "maps for type of instance"
-    default = {
-     us-east-1 = "t2.micro"
-     us-west-2 = "t2.nano"
-    }  
-}
